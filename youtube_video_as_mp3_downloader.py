55622c8c59d12ad5a59f6b6141a88953fc69d51b
Archanciel <jp.schnyder@gmail.com>
2020-08-03 17:58:53 +0200

    bug fix

--- youtube_video_as_mp3_downloader.py
+++ youtube_video_as_mp3_downloader.py
@@ -13,7 +13,7 @@ else:
 playlist = Playlist('https://www.youtube.com/playlist?list=PLzwWSJNcZTMSW-v1x6MhHFKkwrGaEgQ-L')
 playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
-for video in playlist:
+for video in playlist.videos:
 	audioStream = video.streams.get_by_itag(YOUTUBE_STREAM_AUDIO)
 	audioStream.download(output_path=DOWNLOAD_DIR)
 
